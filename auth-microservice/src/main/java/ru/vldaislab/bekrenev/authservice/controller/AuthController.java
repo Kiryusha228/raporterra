41f143d3495c5270571a52b87f6e795fb59b275d
Vladislav <vladislav.bekreneff@yandex.ru>
2025-07-14 15:29:11 +0300

    добавлено хэширование отчетов

--- auth-microservice/src/main/java/ru/vldaislab/bekrenev/authservice/controller/AuthController.java
+++ auth-microservice/src/main/java/ru/vldaislab/bekrenev/authservice/controller/AuthController.java
@@ -1,12 +1,14 @@
 package ru.vldaislab.bekrenev.authservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.vldaislab.bekrenev.authservice.model.dto.AuthRequest;
 import ru.vldaislab.bekrenev.authservice.model.dto.AuthResponse;
 import ru.vldaislab.bekrenev.authservice.model.dto.RegisterRequest;
 import ru.vldaislab.bekrenev.authservice.service.AuthService;
+import ru.vldaislab.bekrenev.authservice.service.MailSenderService;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -14,14 +16,31 @@ import ru.vldaislab.bekrenev.authservice.service.AuthService;
 public class AuthController {
 
     private final AuthService authService;
+    private final MailSenderService mailSender;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authService.register(request));
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        try {
+            AuthResponse response = authService.register(request);
+            mailSender.sendRegistrationEmail(request.getEmail(), request.getFirstName());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Ошибка регистрации: " + e.getMessage());
+        }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
-        return ResponseEntity.ok(authService.authenticate(request));
+    public ResponseEntity<?> authenticate(@RequestBody AuthRequest request) {
+        try {
+            AuthResponse response = authService.authenticate(request);
+            mailSender.sentLoginEmail(request.getEmail());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body("Ошибка входа: " + e.getMessage());
+        }
     }
 }
